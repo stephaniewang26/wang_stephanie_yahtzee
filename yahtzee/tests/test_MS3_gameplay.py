bdbccb0dd625863cb8b42998e86890e467899b17
stephaniewang26 <stephanie.wang26@trinityschoolnyc.org>
2024-10-12 19:33:37 -0400

    10/12 passing all ms3 tests

--- yahtzee/tests/test_MS3_gameplay.py
+++ yahtzee/tests/test_MS3_gameplay.py
@@ -2,63 +2,191 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains 
+from selenium.webdriver.chrome.options import Options
 
 import random
+import json
 
 class Gameplay_Tests(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         #Runs once, before any tests are run
         self.url='http://127.0.0.1:8080/game?username=super_mario&password=123@456!'
-    
+        self.dice_labels= ["blank","one", "two", "three", "four", "five", "six"]
+        self.upper_categories= ["one", "two", "three", "four", "five", "six"]
+        self.lower_categories= ["three_of_a_kind", "four_of_a_kind", "full_house", "small_straight", "large_straight", "yahtzee", "chance"]
+        self.score_elements=["upper_score", "upper_bonus", "upper_total", "lower_score", "upper_total_lower", "grand_total"]
+        self.score_info_finished={
+            "rolls_remaining":0,
+            "upper":{
+                "one":4,
+                "two":8,
+                "three":12,
+                "four":16,
+                "five":20,
+                "six":24
+                },
+                "lower":{
+                    "three_of_a_kind":20,
+                    "four_of_a_kind":26,
+                    "full_house":25,
+                    "small_straight":0,
+                    "large_straight":40,
+                    "yahtzee":50,
+                    "chance":8
+                }
+            }
+
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        options = Options()
+        options.add_argument('--headless=new')
+        self.browser = webdriver.Chrome(options=options)
         self.addCleanup(self.browser.quit)
 
     def test_player_allowed_3_rolls_max(self): 
         self.browser.get(self.url)
+        try:
+            roll_button = self.browser.find_element(By.ID, "roll_button")
+        except:
+            self.fail("roll_button element does not exist!")
+        roll_button.click()
+        roll_button.click()
+        roll_button.click()
 
-        self.assertTrue(False)         
+        current_dice=[]
+        for d in range(5):
+            die_el = self.browser.find_element(By.ID, f"die_{d}")
+            label = die_el.get_attribute("src").split("/")[-1][:-4]
+            current_dice.append(label)
+        
+        roll_button.click()
+
+        for d in range(5):
+            die_el = self.browser.find_element(By.ID, f"die_{d}")
+            label = die_el.get_attribute("src").split("/")[-1][:-4]
             
-        print("_ passed")
+            self.assertTrue(current_dice[d] == label)
+
+        roll_button.click()
+
+        for d in range(5):
+            die_el = self.browser.find_element(By.ID, f"die_{d}")
+            label = die_el.get_attribute("src").split("/")[-1][:-4]
+            
+            self.assertTrue(current_dice[d] == label)
+            
+        print("test_player_allowed_3_rolls_max passed")
     
     def test_player_must_enter_score_even_0(self): 
         self.browser.get(self.url)
+        almost_straigt = [1, 2, 4, 5, 6]
+        self.browser.execute_script(f"window.dice.set({almost_straigt}, 0);")
+        try:
+            roll_button = self.browser.find_element(By.ID, "roll_button")
+        except:
+            self.fail("roll_button element does not exist!")
+        roll_button.click()
 
-        self.assertTrue(False)         
-            
-        print("_ passed")
+        for d in range(5):
+            die_el = self.browser.find_element(By.ID, f"die_{d}")
+            label = die_el.get_attribute("src").split("/")[-1][:-4]
+            self.assertTrue(almost_straigt[d] == self.dice_labels.index(label))  
+
+        roll_button.click()
+
+        for d in range(5):
+            die_el = self.browser.find_element(By.ID, f"die_{d}")
+            label = die_el.get_attribute("src").split("/")[-1][:-4]
+            self.assertTrue(almost_straigt[d] == self.dice_labels.index(label)) 
+        
+        category_input=self.browser.find_element(By.ID, f"one_input")
+        category_input.send_keys(str(1)+Keys.RETURN) 
+        
+        for d in range(5):
+            die_el = self.browser.find_element(By.ID, f"die_{d}")
+            label = die_el.get_attribute("src").split("/")[-1][:-4]
+            self.assertTrue(0 == self.dice_labels.index(label)) 
+
+        print("test_player_must_enter_score_even_0 passed")
     
     def test_prevent_entry_invalid_scores(self): 
         self.browser.get(self.url)
 
-        self.assertTrue(False)         
-            
-        print("_ passed")
+        self.browser.execute_script(f"window.dice.set([3, 3, 6, 3, 6], 1);")
+
+        category_input=self.browser.find_element(By.ID, f"full_house_input")
+        category_input.send_keys(str(21)+Keys.RETURN)          
+        self.assertTrue(category_input.is_enabled())  
+
+        category_input=self.browser.find_element(By.ID, f"three_input")
+        category_input.send_keys(str(21)+Keys.RETURN)          
+        self.assertTrue(category_input.is_enabled())  
+
+        category_input=self.browser.find_element(By.ID, f"chance_input")
+        category_input.send_keys(str(21)+Keys.RETURN)          
+        self.assertFalse(category_input.is_enabled()) 
+
+        print("test_prevent_entry_invalid_scores passed")
     
     def test_prevent_dice_rolls_for_finished_games(self): 
         self.browser.get(self.url)
 
-        self.assertTrue(False)         
+        self.browser.execute_script(f"window.gamecard.load_scorecard(JSON.parse('{json.dumps(self.score_info_finished)}'));")
+        current_dice=[]
+        for d in range(5):
+            die_el = self.browser.find_element(By.ID, f"die_{d}")
+            label = die_el.get_attribute("src").split("/")[-1][:-4]
+            current_dice.append(label)
+
+        try:
+            roll_button = self.browser.find_element(By.ID, "roll_button")
+        except:
+            self.fail("roll_button element does not exist!")
+        roll_button.click()
+
+        for d in range(5):
+            die_el = self.browser.find_element(By.ID, f"die_{d}")
+            label = die_el.get_attribute("src").split("/")[-1][:-4]
             
-        print("_ passed")
+            self.assertTrue(current_dice[d] == label)  
+        
+        roll_button.click()
+
+        for d in range(5):
+            die_el = self.browser.find_element(By.ID, f"die_{d}")
+            label = die_el.get_attribute("src").split("/")[-1][:-4]
+            
+            self.assertTrue(current_dice[d] == label)  
+            
+        print("test_prevent_dice_rolls_for_finished_games passed")
     
     def test_dice_reset_after_valid_score(self): 
         # blank and unreserved
         self.browser.get(self.url)
+        self.browser.execute_script(f"window.dice.set([3, 3, 6, 3, 6], 1);")
+        category_input=self.browser.find_element(By.ID, f"full_house_input")
+        category_input.send_keys(str(25)+Keys.RETURN) 
 
-        self.assertTrue(False)         
+        for d in range(5):
+            die_el = self.browser.find_element(By.ID, f"die_{d}")
+            self.assertTrue("blank" in die_el.get_attribute("src"))  
+            self.assertTrue("reserved" not in die_el.get_attribute("class"))  
             
-        print("_ passed")
+        print("test_dice_reset_after_valid_score passed")
     
     def test_new_game_blank_totals_and_categories(self): 
-        # blank and unreserved
         self.browser.get(self.url)
 
-        self.assertTrue(False)         
-            
-        print("_ passed")
+        for category in self.lower_categories+self.upper_categories:
+            category_el = self.browser.find_element(By.ID, f"{category}_input")
+            self.assertTrue(category_el.is_enabled())
+            self.assertTrue(category_el.get_attribute("value")=="")       
+        
+        for score in self.score_elements:
+            score_el = self.browser.find_element(By.ID, f"{score}")
+            self.assertTrue(score_el.text=="") 
+
+        print("test_new_game_blank_totals_and_categories passed")
 
 
 if __name__ == '__main__':
--- yahtzee/tests/test_MS3_save_load_UI.py
+++ yahtzee/tests/test_MS3_save_load_UI.py
@@ -2,47 +2,116 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains 
 
 import random
+import json
 
 class Save_Load_UI_Tests(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         #Runs once, before any tests are run
         self.url='http://127.0.0.1:8080/game?username=super_mario&password=123@456!'
-    
+        self.upper_categories= ["one", "two", "three", "four", "five", "six"]
+        self.lower_categories= ["three_of_a_kind", "four_of_a_kind", "full_house", "small_straight", "large_straight", "yahtzee", "chance"]
+        self.score_elements=["upper_score", "upper_bonus", "upper_total", "lower_score", "upper_total_lower", "grand_total"]
+        self.score_info_partial_bonus={
+            "rolls_remaining":1,
+            "upper":{
+                "one":4,
+                "two":8,
+                "three":-1,
+                "four":16,
+                "five":20,
+                "six":24
+            },
+            "lower":{
+                "three_of_a_kind":-1,
+                "four_of_a_kind":26,
+                "full_house":-1,
+                "small_straight":0,
+                "large_straight":40,
+                "yahtzee":0,
+                "chance":8
+            }
+        }
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.addCleanup(self.browser.quit)
 
     def test_save_incomplete_game(self): 
         self.browser.get(self.url)
+        self.browser.execute_script(f"localStorage.removeItem('yahtzee');")#clear any old games
 
-        self.assertTrue(False) #0: blank.svg         
-            
-        print("_ passed")
+        self.browser.execute_script(f"window.gamecard.load_scorecard(JSON.parse('{json.dumps(self.score_info_partial_bonus)}'));")
+        load_button=self.browser.find_element(By.ID, f"save_game")
+        load_button.click()
+
+        game = self.browser.execute_script(f"return localStorage.getItem('yahtzee');")
+        self.assertTrue(game is not None) #a game with key='yahtzee' should exist          
+        self.assertTrue(json.loads(game) == self.score_info_partial_bonus)        
+        print("test_save_incomplete_game passed")
     
     def test_only_save_scores_for_disabled_categories(self): 
         self.browser.get(self.url)
+        self.browser.execute_script(f"localStorage.removeItem('yahtzee');")#clear any old games
 
-        self.assertTrue(False) #0: blank.svg         
-            
-        print("_ passed")
+        self.browser.execute_script(f"window.gamecard.load_scorecard(JSON.parse('{json.dumps(self.score_info_partial_bonus)}'));")
+        
+        #type input, but don't hit enter
+        self.browser.execute_script(f"window.dice.set([3, 3, 3, 3, 3], 1);")
+        category_input=self.browser.find_element(By.ID, f"three_input")
+        category_input.send_keys(str(15)) 
+        category_input=self.browser.find_element(By.ID, f"three_of_a_kind_input")
+        category_input.send_keys(str(15))
+        
+        load_button=self.browser.find_element(By.ID, f"save_game")
+        load_button.click()
+
+        game = self.browser.execute_script(f"return localStorage.getItem('yahtzee');")
+        self.assertTrue(game is not None) #a game with key='yahtzee' should exist          
+        self.assertTrue(json.loads(game) == self.score_info_partial_bonus) #only disabled categories are saved       
+        print("test_only_save_scores_for_disabled_categories passed")
     
     def test_load_and_disable_categories(self): 
         self.browser.get(self.url)
+        self.browser.execute_script(f"window.gamecard.load_scorecard(JSON.parse('{json.dumps(self.score_info_partial_bonus)}'));")
 
-        self.assertTrue(False) #0: blank.svg         
-            
-        print("_ passed")
+        rolls = self.browser.execute_script(f"return window.dice.get_rolls_remaining();")
+        self.assertTrue(rolls == self.score_info_partial_bonus["rolls_remaining"]) 
+        for lower_cat in self.lower_categories:
+            cat_el = self.browser.find_element(By.ID, f"{lower_cat+'_input'}")
+            if self.score_info_partial_bonus["lower"][lower_cat] >= 0:
+                self.assertFalse(cat_el.is_enabled())
+                self.assertTrue(cat_el.get_attribute("value") == str(self.score_info_partial_bonus["lower"][lower_cat]))
+            else:
+                self.assertTrue(cat_el.is_enabled()) 
+                self.assertTrue(cat_el.text == "")
+        
+        for upper_cat in self.upper_categories:
+            cat_el = self.browser.find_element(By.ID, f"{upper_cat+'_input'}")
+            if self.score_info_partial_bonus["upper"][upper_cat] >= 0:
+                self.assertFalse(cat_el.is_enabled())
+                self.assertTrue(cat_el.get_attribute("value") == str(self.score_info_partial_bonus["upper"][upper_cat]))
+            else:
+                self.assertTrue(cat_el.is_enabled()) 
+                self.assertTrue(cat_el.text == "")
+
+        print("test_load_and_disable_categories passed")
     
     def test_no_error_with_load_nonexistent_game(self): 
         self.browser.get(self.url)
+        self.browser.execute_script(f"localStorage.removeItem('yahtzee');")#clear any old games
 
-        self.assertTrue(False) #0: blank.svg         
+        self.browser.execute_script(f"window.gamecard.load_scorecard(JSON.parse('{json.dumps(self.score_info_partial_bonus)}'));")
+
+        load_button=self.browser.find_element(By.ID, f"load_game")
+        load_button.click()
+
+        for log in self.browser.get_log('browser'): 
+            if "favicon" not in log['message']:
+                self.assertTrue(False)  #causes an error that isn't a missing favicon        
             
-        print("_ passed")
+        print("test_no_error_with_load_nonexistent_game passed")
 
 if __name__ == '__main__':
     unittest.main()
